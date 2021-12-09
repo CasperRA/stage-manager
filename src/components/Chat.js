10dd2099e86edd289f228d9bc8b4b1b2e6e8c915
TheSpahThatSpies <christianborggaard@gmail.com>
2021-12-09 09:25:13 +0100

    Chat Startup
    Fungerende inputfelt til tekst og billeder lavet med Chat Engine

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "../styles/global.css";
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from '../components/ChatFeed'
 
 function Chat() {
   return (
-    <div>
-      <h1>Chat Loaded</h1>
-    </div>
+      <ChatEngine
+        projectID='0e50bec2-4943-41cc-ac42-8d2e2b2d8abf'
+        userName='Christian Borggård'
+        userSecret='thisisme'
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
   );
 }
 
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,78 @@
+import React, { Component }  from 'react';
+import { render } from 'react-dom';
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
+const ChatFeed = (props) => {
+    const { chats, activeChat, userName, messages } = props;
+
+    //Find nuværende chatrum
+    const chat = chats && chats[activeChat];
+
+    //Hvis besked er læst
+    const renderReadReceipts = (message, isMyMessage) => {
+        chat.people.map((person, index) => person.last_read == message.id && (
+            <div 
+                key={`read_${index}`}
+                className='read-receipt'
+                style={{
+                    float: isMyMessage ? 'right' : 'left',
+                    backgroundimage: `url(${person?.person?.avatar})`
+                }}
+            />
+        ))
+    }
+
+    //Besked generering
+    const renderMessages = () => {
+        //Hent besked ID
+        const keys = Object.keys(messages);
+
+        //Render beskeder
+        return keys.map((key, index) => {
+            const message = messages[key];
+            // Hvis der er beskeder, find seneste besked
+            const lastMessageKey = index == 0 ? null : keys[index - 1];
+            // Find ud af om besked er sendt fra bruger
+            const isMyMessage = userName == message.sender.username;
+
+            //Beskeder
+            return (
+                <div key={`msg_${index}`} style={{ width: '100%'}}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ? <MyMessage message={message}/>
+                            : <TheirMessage message={message} lastMessage={messages[lastMessageKey]}/>
+                        }
+                        <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+                            read-receipts
+                        </div>
+                    </div>
+                </div>
+            );
+        })
+    }
+
+    if(!chat) return 'Loading...';
+
+    //Chat feed struktur
+    return (
+        <div className="chat-feed">
+            <div className="chat-title-container"> 
+                <div className="chat-title">{chat.title}</div>
+                <div className="chat-subtitle">
+                    {chat.people.map((person) => ` ${person.person.username}`)}
+                </div>
+            </div>
+            {renderMessages()}
+            <div style={{ height: '100px' }} />
+            <div className="message-form-container">
+                <MessageForm {...props} chatId={activeChat} />
+            </div>
+        </div>
+    );
+}
+
+export default ChatFeed;
