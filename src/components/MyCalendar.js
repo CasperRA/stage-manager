8e6f7df5026e57c8dc9237ef1d10a65fc78ed91a
Casper <casp8249@edu.easj.dk>
2021-12-13 05:55:12 +0100

    creating events

--- src/components/MyCalendar.js
+++ src/components/MyCalendar.js
@@ -1,39 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Calendar from "react-awesome-calendar";
 import { CirclePicker } from "react-color";
 
-const events = [
-  {
-    id: 1,
-    color: "#fd3153",
-    from: "2021-12-22T08:00+00:00",
-    to: "2021-12-22T12:00:00+00:00",
-    title: "Tech Day",
-  },
-  {
-    id: 2,
-    color: "#1ccb9e",
-    from: "2021-12-13T09:00:00+00:00",
-    to: "2021-12-13T14:00:00+00:00",
-    title: "Practice",
-  },
-  {
-    id: 3,
-    color: "#1ccb9e",
-    from: "2021-12-13T09:00:00+00:00",
-    to: "2021-12-13T14:00:00+00:00",
-    title: "Practice",
-  },
-  {
-    id: 4,
-    color: "#1ccb9e",
-    from: "2021-12-16T09:00:00+00:00",
-    to: "2021-12-16T14:00:00+00:00",
-    title: "Practice",
-  },
-];
+const events = [];
 
 function MyCalendar() {
+  let eventColor = 0;
+  let eventTitle = 0;
+  let startDate = 0;
+  let endDate = 0;
+
   function startEventCreation() {
     document.getElementById("eventCreationContainer").style.display = "block";
   }
@@ -42,31 +18,43 @@ function MyCalendar() {
     document.getElementById("eventCreationContainer").style.display = "none";
   }
 
-  function setDate() {
-    var today = new Date();
-    var date =
-      today.getFullYear() +
-      "-" +
-      (today.getMonth() + 1) +
-      "-" +
-      today.getDate();
-
-    if (today.getDate() >= 0 && today.getDate() <= 9) {
-      date =
-        today.getFullYear() +
-        "-" +
-        (today.getMonth() + 1) +
-        "-0" +
-        today.getDate();
-    }
-    inputDate = date;
+  function setColor(color) {
+    eventColor = color.hex;
+    console.log(eventColor);
   }
-  setDate();
-  let inputDate;
 
-  function changeInputDate(event) {
-    console.log(event.target.value);
-    document.getElementById("startDate").value = event.target.value;
+  function setTitle(event) {
+    eventTitle = event.target.value;
+    console.log(eventTitle);
+  }
+
+  function setStartDate(event) {
+    startDate = event.target.value;
+    console.log(startDate);
+  }
+
+  function setEndDate(event) {
+    endDate = event.target.value;
+    console.log(endDate);
+  }
+
+  function createEvent() {
+    if (
+      eventColor !== 0 &&
+      eventTitle !== 0 &&
+      startDate !== 0 &&
+      endDate !== 0
+    ) {
+      events.push({
+        color: eventColor,
+        from: startDate,
+        to: endDate,
+        title: eventTitle,
+      });
+      console.log("accepted");
+    } else {
+      alert("You are missing something");
+    }
   }
 
   return (
@@ -88,40 +76,38 @@ function MyCalendar() {
           <div>
             <h1>Create an Event</h1>
           </div>
-          <form className="eventForm">
+          <div className="eventForm">
             <h3>Event Name:</h3>
             <input
               type="text"
               placeholder="Event Example"
               className="eventInput"
+              onChange={setTitle}
             />
             <h3>Start date:</h3>
             <input
-              type="date"
+              type="datetime-local"
               id="startDate"
               className="eventInput"
               name="trip-start"
-              value={inputDate}
-              min="2021-01-01"
-              max="2040-12-31"
-              onChange={changeInputDate}
+              onChange={setStartDate}
             />
             <h3>End date:</h3>
             <input
-              type="date"
+              type="datetime-local"
               id="endDate"
               className="eventInput"
-              name="trip-end"
-              value={inputDate}
-              min="2021-01-01"
-              max="2040-12-31"
-              onChange={changeInputDate}
+              name="trip-start"
+              onChange={setEndDate}
             />
             <div className="colorContainer">
               <h3>Pick a color</h3>
-              <CirclePicker />
+              <CirclePicker onChange={setColor} />
             </div>
-          </form>
+            <button className="finalizeEvent" onClick={createEvent}>
+              Create
+            </button>
+          </div>
         </div>
       </div>
     </div>
