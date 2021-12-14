42b6886a8788dcceef0fd2a4155993a08ecef09c
TheSpahThatSpies <christianborggaard@gmail.com>
2021-12-14 11:13:36 +0100

    Login system

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,13 +2,16 @@ import React from "react";
 import "../styles/global.css";
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from '../components/ChatFeed'
+import LoginForm from '../components/LoginForm'
 
 function Chat() {
+  if(!localStorage.getItem('username')) return <LoginForm />
+
   return (
       <ChatEngine
         projectID='0e50bec2-4943-41cc-ac42-8d2e2b2d8abf'
-        userName='Christian Borggård'
-        userSecret='thisisme'
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
       />
   );
