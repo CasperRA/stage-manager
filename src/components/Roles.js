2a77ad6fbe4260d78b2dacfc3a9ecdb22a6290d9
Casper <casp8249@edu.easj.dk>
2021-12-16 23:34:01 +0100

    Role/team styling

--- src/components/Roles.js
+++ src/components/Roles.js
@@ -6,55 +6,90 @@ function Roles() {
     <div>
       <div className="rolesContainer">
         <div className="rolesPadding">
-          <h1>Roles</h1>
-          <h3>Director</h3>
-          <ul className="rolesList">
-            <li>
-              <img src={ProfilePic} alt="" />
-              <p>William</p>
-            </li>
-          </ul>
-          <h3>Stage Manager</h3>
-          <ul className="rolesList">
-            <li>
-              <img src={ProfilePic} alt="" />
-              <p>Stella</p>
-            </li>
-          </ul>
-          <h3>Choreographer</h3>
-          <ul className="rolesList">
-            <li>
-              <img src={ProfilePic} alt="" />
-              <p>Sarah</p>
-            </li>
-          </ul>
-          <h3>Actors</h3>
-          <ul className="rolesList">
-            <li>
-              <img src={ProfilePic} alt="" />
-              <p>Michael</p>
-            </li>
-            <li>
-              <img src={ProfilePic} alt="" />
-              <p>Michael</p>
-            </li>
-            <li>
-              <img src={ProfilePic} alt="" />
-              <p>Michael</p>
-            </li>
-            <li>
-              <img src={ProfilePic} alt="" />
-              <p>Cleo</p>
-            </li>
-            <li>
-              <img src={ProfilePic} alt="" />
-              <p>Cleo</p>
-            </li>
-            <li>
-              <img src={ProfilePic} alt="" />
-              <p>Cleo</p>
-            </li>
-          </ul>
+          <h1 className="rolesTitle">Team</h1>
+          <div className="roleWrapper">
+            <h3>Director</h3>
+            <ul className="rolesList">
+              <li>
+                <img src={ProfilePic} alt="" />
+                <div>
+                  <p>William</p>
+                  <p className="roleUnderName">Director</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="roleWrapper">
+            <h3>Stage Manager</h3>
+            <ul className="rolesList">
+              <li>
+                <img src={ProfilePic} alt="" />
+                <div>
+                  <p>William</p>
+                  <p className="roleUnderName">Director</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="roleWrapper">
+            <h3>Choreographer</h3>
+            <ul className="rolesList">
+              <li>
+                <img src={ProfilePic} alt="" />
+                <div>
+                  <p>William</p>
+                  <p className="roleUnderName">Director</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="roleWrapper">
+            <h3>Actors</h3>
+            <ul className="rolesList">
+              <li>
+                <img src={ProfilePic} alt="" />
+                <div>
+                  <p>William</p>
+                  <p className="roleUnderName">Director</p>
+                </div>
+              </li>
+              <li>
+                <img src={ProfilePic} alt="" />
+                <div>
+                  <p>William</p>
+                  <p className="roleUnderName">Director</p>
+                </div>
+              </li>
+              <li>
+                <img src={ProfilePic} alt="" />
+                <div>
+                  <p>William</p>
+                  <p className="roleUnderName">Director</p>
+                </div>
+              </li>
+              <li>
+                <img src={ProfilePic} alt="" />
+                <div>
+                  <p>William</p>
+                  <p className="roleUnderName">Director</p>
+                </div>
+              </li>
+              <li>
+                <img src={ProfilePic} alt="" />
+                <div>
+                  <p>William</p>
+                  <p className="roleUnderName">Director</p>
+                </div>
+              </li>
+              <li>
+                <img src={ProfilePic} alt="" />
+                <div>
+                  <p>William</p>
+                  <p className="roleUnderName">Director</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
